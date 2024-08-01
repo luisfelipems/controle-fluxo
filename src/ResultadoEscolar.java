7ca72199b74fe7e62d9b81fdd31ac143c164d158
Felipe <luisfelipe.martinssilva@gmail.com>
2024-08-01 20:40:41 -0300

    Completmentando a expressão ternária

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -4,12 +4,14 @@ public class ResultadoEscolar {
         
         int nota = 6;
        
-        String resultado = nota >= 7 ? "Aprovado" : "Repovado";
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		
         /* 
         * Declarando a variável resultado e atribuindo o seu valor de acordo com 
         * a condição da expressão ternária. Se a nota for maior ou igual a 7, a 
-        * variável receberá o valor de "Aprovado" caso contrário, o valor da variável
-        * resultado será "Reprovado".
+        * variável receberá o valor de "Aprovado", se a nota for menor que 7 e
+        * maior ou igual a 5, o valor de resultado será "Recuperação". Caso contrário,
+        * o valor da variável resultado será "Reprovado".
         */
 
         System.out.println(resultado); // Impressão do valor da variável resultado.
