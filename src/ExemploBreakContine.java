03be64f8d49d32a02288a21ca151cffb46292d81
Felipe <luisfelipe.martinssilva@gmail.com>
2024-08-02 21:48:12 -0300

    Adicionando arquivos com a utilização de While. DoWhile e FOR

---
+++ src/ExemploBreakContine.java
@@ -0,0 +1,15 @@
+public class ExemploBreakContine {
+    
+    public static void main(String[] args) {
+        
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+             
+            // break; interrompe o laço por completo.
+            // continue; ignora a instrução seguinte "Pulando" e volta o laço para continua após o número receber o valor 3.
+            
+            System.out.println(numero);
+            
+        }
+    }
+}
